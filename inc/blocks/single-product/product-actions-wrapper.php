003f1a07c4251b85fc0f8fe37a3ff40bf95eda90
reza khorrami <rezakhorrami729@gmail.com>
2018-10-24 16:04:14 +0330

    translate page product-actions-wrapper.php

--- inc/blocks/single-product/product-actions-wrapper.php
+++ inc/blocks/single-product/product-actions-wrapper.php
@@ -1,17 +1,17 @@
 <div class="product-actions-wrapper">
 	<div class="product-actions">
 		<div class="availability">
-			Availability: <p class="stock in-stock">1000 in stock</p>
+		دسترسی: <p class="stock in-stock">1000 در انبار</p>
 		</div><!-- .availability -->	
 
 
 		<div class="additional-info">
-			<i class="tm tm-free-delivery"></i>Item with <strong>Free Delivery</strong>
+			<i class="tm tm-free-delivery"></i> مورد با <strong>تحویل رایگان</strong>
 		</div><!-- .additional-info -->	
 
 		<p class="price">
 			<span class="woocommerce-Price-amount amount">
-			<span class="woocommerce-Price-currencySymbol">$</span>2457.00</span> 
+			<span class="woocommerce-Price-currencySymbol">تومان</span>2457.00</span> 
 		</p><!-- .price -->
 
 		
@@ -20,16 +20,16 @@
 				<tbody>
 					<tr>
 						<td class="label">
-							<label for="pa_screen-size">Screen Size</label>
+							<label for="pa_screen-size">اندازه صفحه نمایش</label>
 						</td>
 
 						<td class="value">
 							<select data-show_option_none="yes" data-attribute_name="attribute_pa_screen-size" name="attribute_pa_screen-size" class="" id="pa_screen-size">
-								<option value="">Choose an option</option>
-								<option value="45-inch" class="attached enabled">45 Inch</option>
-								<option value="60-inch" class="attached enabled">60 Inch</option>
+								<option value="">یک گزینه را انتخاب کنید</option>
+								<option value="45-اینچ" class="attached enabled">45 اینچ</option>
+								<option value="60-اینچ" class="attached enabled">60 اینچ</option>
 							</select>
-							<a href="#" class="reset_variations" style="visibility: hidden;">Clear</a>
+							<a href="#" class="reset_variations" style="visibility: hidden;">پاک کردن</a>
 						</td>
 					</tr>
 				</tbody>
@@ -38,15 +38,15 @@
 			<div class="single_variation_wrap">
 				
 				<div class="woocommerce-variation-add-to-cart variations_button woocommerce-variation-add-to-cart-disabled">
-					<div class="quantity"> <label for="quantity-input">Quantity</label> <input id="quantity-input" type="number" name="quantity" value="1" title="Qty" class="input-text qty text" size="4"></div>
+					<div class="quantity"> <label for="quantity-input">مقدار</label> <input id="quantity-input" type="number" name="quantity" value="1" title="Qty" class="input-text qty text" size="4"></div>
 
-					<button class="single_add_to_cart_button button alt wc-variation-selection-needed" type="submit">Add to cart</button>
+					<button class="single_add_to_cart_button button alt wc-variation-selection-needed" type="submit">افزودن به سبد خرید</button>
 					<input type="hidden" value="2471" name="add-to-cart">
 					<input type="hidden" value="2471" name="product_id">
 					<input type="hidden" value="0" class="variation_id" name="variation_id">
 				</div>
 			</div><!-- .single_variation_wrap -->
 		</form><!-- .variations_form -->
-		<a class="add-to-compare-link" href="index.php?page=compare">Add to compare</a>
+		<a class="add-to-compare-link" href="index.php?page=compare">افزودن برای مقایسه</a>
 	</div><!-- .product-actions -->
 </div><!-- .product-actions-wrapper -->
