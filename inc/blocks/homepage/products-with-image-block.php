39ab544ba6fbc2eb940fee17ab74ce64f2e897ff
mohammadreza peykhosh <mohammadreza.peikhosh@gmail.com>
2018-10-24 06:30:20 -0700

    landing-v1.php translate

--- inc/blocks/homepage/products-with-image-block.php
+++ inc/blocks/homepage/products-with-image-block.php
@@ -8,8 +8,8 @@ $column = 5;
 		<div class="col-full">
 			<div class="row">
 				<div class="product-info">
-					<h2 class="section-title">Desktops PCs</h2>
-					<div class="description">Donec id malesuada elit. Donec tempor sit amet est ac blandit. Phasellus ac sem nisl. Vestibulum aliquam, ligula in pretium congue, massa felis ultrices metus, nec mattis elit diam lobortis justo. Ut dapibus gravida eros ac bibendum. Phasellus ac tempus libero. Mauris eleifend, mi at viverra scelerisque, dolor leo luctus justo, id fermentum tellus nisl eget est.</div>
+					<h2 class="section-title">کامپیوتر های رومیزی</h2>
+					<div class="description">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. </div>
 				</div>
 				<div class="product-image">
 					<img alt="" src="assets/images/products/landing-1.png">
@@ -38,16 +38,16 @@ $column = 5;
                     <a class="woocommerce-LoopProduct-link woocommerce-loop-product__link" href="index.php?page=single-product-fullwidth">
                         <img width="224" height="197" alt="" class="attachment-shop_catalog size-shop_catalog wp-post-image" src="<?php echo $productImage[$i]['product_image'] ?>">
                         <span class="price">
-                            <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>57.99</span>
+                            <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">هزار تومان</span>570</span>
                         </span><!-- .price -->
 
-                        <h2 class="woocommerce-loop-product__title">On-ear Wireless NXTG</h2>
+                        <h2 class="woocommerce-loop-product__title">بی سیم NXTG بی سیم</h2>
                     </a><!-- .woocommerce-LoopProduct-link -->
-                    <div class="techmarket-product-rating"><div class="star-rating" title="Rated 0 out of 5"><span style="width:0%"><strong class="rating">0</strong> out of 5</span></div> <span class="review-count">(0)</span></div>
+                    <div class="techmarket-product-rating"><div class="star-rating" title="Rated 0 out of 5"><span style="width:0%"><strong class="rating">0</strong> از 5</span></div> <span class="review-count">(0)</span></div>
 
                     <div class="hover-area">
-                        <a class="button" href="index.php?page=cart">Add to cart</a>
-                        <a class="add-to-compare-link" href="index.php?page=compare">Add to compare</a>
+                        <a class="button" href="index.php?page=cart">افزودن به سبد خرید</a>
+                        <a class="add-to-compare-link" href="index.php?page=compare">افزودن برای مقایسه</a>
                     </div><!-- .hover-area -->
                 </div><!-- .product -->
             <?php } ?>
@@ -55,7 +55,7 @@ $column = 5;
         </div><!-- .products -->
     </div><!-- .woocommerce -->
 
-	<a href="index.php?page=shop" class="load-more-button">Shop All Desktops PCs</a>
+	<a href="index.php?page=shop" class="load-more-button">فروشگاه تمام رایانه های رومیزی</a>
 </section><!-- .section-products-with-image -->
 
 <section class="section-products-with-image">
@@ -63,8 +63,8 @@ $column = 5;
 		<div class="col-full">
 			<div class="row">
 				<div class="product-info">
-					<h2 class="section-title">Gaming Laptops</h2>
-					<div class="description">Donec id malesuada elit. Donec tempor sit amet est ac blandit. Phasellus ac sem nisl. Vestibulum aliquam, ligula in pretium congue, massa felis ultrices metus, nec mattis elit diam lobortis justo. Ut dapibus gravida eros ac bibendum. Phasellus ac tempus libero. Mauris eleifend, mi at viverra scelerisque, dolor leo luctus justo, id fermentum tellus nisl eget est.</div>
+					<h2 class="section-title">لپ تاپ های بازی</h2>
+					<div class="description">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </div>
 				</div>
 				<div class="product-image">
 					<img alt="" src="assets/images/products/landing-1.1.png">
@@ -94,16 +94,16 @@ $column = 5;
                         <img width="224" height="197" alt="" class="attachment-shop_catalog size-shop_catalog wp-post-image" src="<?php echo $productImage[$i]['product_image'] ?>">
                         
                         <span class="price">
-                            <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>57.99</span>
+                            <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">هزار تومان</span>570</span>
                         </span><!-- .price -->
 
-                        <h2 class="woocommerce-loop-product__title">On-ear Wireless NXTG</h2>
+                        <h2 class="woocommerce-loop-product__title">بی سیم NXTG بی سیم</h2>
                     </a><!-- .woocommerce-LoopProduct-link -->
                     <div class="techmarket-product-rating"><div class="star-rating" title="Rated 0 out of 5"><span style="width:0%"><strong class="rating">0</strong> out of 5</span></div> <span class="review-count">(0)</span></div>
 
                     <div class="hover-area">
-                        <a class="button" href="index.php?page=cart">Add to cart</a>
-                        <a class="add-to-compare-link" href="index.php?page=compare">Add to compare</a>
+                        <a class="button" href="index.php?page=cart">افزودن به سبد خرید</a>
+                        <a class="add-to-compare-link" href="index.php?page=compare">افزودن برای مقایسه</a>
                     </div><!-- .hover-area -->
                 </div><!-- .product -->
             <?php } ?>
@@ -112,7 +112,7 @@ $column = 5;
     </div><!-- .woocommerce -->
 	
 
-	<a href="index.php?page=shop" class="load-more-button">Shop All Desktops PCs</a>
+	<a href="index.php?page=shop" class="load-more-button">فروشگاه تمام رایانه های رومیزی</a>
 </section><!-- .section-products-with-image -->
 
 <section class="section-products-with-image">
@@ -120,8 +120,8 @@ $column = 5;
 		<div class="col-full">
 			<div class="row">
 				<div class="product-info">
-					<h2 class="section-title">Accesories</h2>
-					<div class="description">Donec id malesuada elit. Donec tempor sit amet est ac blandit. Phasellus ac sem nisl. Vestibulum aliquam, ligula in pretium congue, massa felis ultrices metus, nec mattis elit diam lobortis justo. Ut dapibus gravida eros ac bibendum. Phasellus ac tempus libero. Mauris eleifend, mi at viverra scelerisque, dolor leo luctus justo, id fermentum tellus nisl eget est.</div>
+					<h2 class="section-title">اکسسوری</h2>
+					<div class="description">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </div>
 				</div>
 				<div class="product-image">
 					<img alt="" src="assets/images/products/landing-1.2.png">
@@ -154,13 +154,13 @@ $column = 5;
                             <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>57.99</span>
                         </span><!-- .price -->
                         
-                        <h2 class="woocommerce-loop-product__title">On-ear Wireless NXTG</h2>
+                        <h2 class="woocommerce-loop-product__title">بی سیم NXTG بی سیم</h2>
                     </a><!-- .woocommerce-LoopProduct-link -->
                     <div class="techmarket-product-rating"><div class="star-rating" title="Rated 0 out of 5"><span style="width:0%"><strong class="rating">0</strong> out of 5</span></div> <span class="review-count">(0)</span></div>
 
                     <div class="hover-area">
-                        <a class="button" href="index.php?page=cart">Add to cart</a>
-                        <a class="add-to-compare-link" href="index.php?page=compare">Add to compare</a>
+                        <a class="button" href="index.php?page=cart">افزودن به سبد خرید</a>
+                        <a class="add-to-compare-link" href="index.php?page=compare">افزودن برای مقایسه</a>
                     </div><!-- .hover-area -->
                 </div><!-- .product -->
             <?php } ?>
@@ -168,5 +168,5 @@ $column = 5;
         </div><!-- .products -->
     </div><!-- .woocommerce -->
 
-	<a href="index.php?page=shop" class="load-more-button">Shop All Desktops PCs</a>
+	<a href="index.php?page=shop" class="load-more-button">فروشگاه تمام رایانه های رومیزی</a>
 </section><!-- .section-products-with-image -->
--- inc/blocks/homepage/slider/landing-v1-slider.php
+++ inc/blocks/homepage/slider/landing-v1-slider.php
@@ -2,10 +2,10 @@
 	<div class="slider-1">
 		<img src="assets/images/slider/landing-v1-img-1.png" alt="">
 		<div class="caption">
-			<div class="pre-title">Gaming 4K*</div>
-			<div class="title">Desktops & Laptops</div>
+			<div class="pre-title">بازی 4K *</div>
+			<div class="title">لب تاپ و کامپیوتر رومیزی</div>
 			<div class="sub-title">#GamingTechMarket</div>
-			<div class="bottom-caption">*Only Desktops PC are for Gaming purposes</div>
+			<div class="bottom-caption">* فقط کامپیوترهای رومیزی برای اهداف بازی هستند</div>
 		</div>
 	</div>
 </div>
