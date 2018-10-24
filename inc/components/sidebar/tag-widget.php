0fb9ac2e0259b20f93c759d87ed19809fb8b2896
m.hadi.zarrabi <m.hadi.z.1997@gmail.com>
2018-10-24 16:15:07 +0330

    proj

--- inc/components/sidebar/tag-widget.php
+++ inc/components/sidebar/tag-widget.php
@@ -1,14 +1,14 @@
 <div class="widget widget_tag_cloud">
-	<span class="gamma widget-title">Tags Clouds</span>
+	<span class="gamma widget-title">برچسپ ها</span>
 	<div class="tagcloud">
-		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">amazon like</a>
-		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">Awesome</a>
+		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">amazon لینک سایت </a>
+		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">عالی</a>
 		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">bootstrap</a>
-		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">buy it</a>
-		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">clean design</a>
-		<a style="font-size: 8pt;" class="tag-cloud-link" href="#">electronics</a>
-		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">theme</a>
-		<a style="font-size: 8pt;" class="tag-cloud-link" href="#">video post format</a>
+		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">خرید</a>
+		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">طراحی تمیز</a>
+		<a style="font-size: 8pt;" class="tag-cloud-link" href="#">الکترونیک</a>
+		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">موضوع</a>
+		<a style="font-size: 8pt;" class="tag-cloud-link" href="#">فرمت پست ویدئو</a>
 		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">woocommerce</a>
 		<a style="font-size: 22pt;" class="tag-cloud-link" href="#">wordpress</a>
 	</div><!-- .tagcloud -->
