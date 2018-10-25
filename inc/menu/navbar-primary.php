4a4029a666b2fb63fa8454343aac76bda57afb01
mahdihasannejad <mahdi2061378@gmail.com>
2018-10-25 15:49:44 +0330

    full translate folder menu

--- inc/menu/navbar-primary.php
+++ inc/menu/navbar-primary.php
@@ -1,39 +1,39 @@
 <nav id="navbar-primary" class="navbar-primary" aria-label="Navbar Primary" data-nav="flex-menu">
 	<ul id="menu-navbar-primary" class="nav yamm">
 		<li class="menu-item animate-dropdown">
-			<a title="ALL CATEGORIES" href="index.php?page=product-category">ALL CATEGORIES</a>
+			<a title="ALL CATEGORIES" href="index.php?page=product-category"> دسته بندی ها</a>
 		</li>
 
 		<li class="menu-item animate-dropdown">
-			<a title="COMPUTERS &amp; LAPTOPS" href="index.php?page=product-category">COMPUTERS &#038; LAPTOPS</a>
+			<a title="COMPUTERS &amp; LAPTOPS" href="index.php?page=product-category">کامپیوتر و لپتاپ </a>
 		</li>
 
 		<li class="yamm-fw menu-item menu-item-has-children animate-dropdown dropdown">
-			<a title="Pages" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" href="#">Pages <span class="caret"></span></a>
+			<a title="Pages" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" href="#">صفحه <span class="caret"></span></a>
 			<?php require_once 'inc/menu/mega-menu.php'; ?>
 		</li>
 
 		<li class="menu-item animate-dropdown">
-			<a title="CAMERAS &amp; PHOTO" href="index.php?page=product-category">CAMERAS &#038; PHOTO</a>
+			<a title="CAMERAS &amp; PHOTO" href="index.php?page=product-category">دروبین و عکاسی</a>
 		</li>
 
 		<li class="menu-item animate-dropdown">
-			<a title="PHONES &amp; TABLETS" href="index.php?page=product-category">PHONES &#038; TABLETS</a>
+			<a title="PHONES &amp; TABLETS" href="index.php?page=product-category">تلفن هوشمند و تبلت</a>
 		</li>
 
 		<li class="menu-item animate-dropdown">
-			<a title="GAMES &amp; CONSOLES" href="index.php?page=product-category">GAMES &#038; CONSOLES</a>
+			<a title="GAMES &amp; CONSOLES" href="index.php?page=product-category">بازی ویدیوی و کنسول بازی </a>
 		</li>
 
 		<li class="menu-item animate-dropdown">
-			<a title="TV &amp; AUDIO" href="index.php?page=product-category">TV &#038; AUDIO</a>
+			<a title="TV &amp; AUDIO" href="index.php?page=product-category">تلوزیون و سینما خانگی</a>
 		</li>
 
 		<li class="menu-item animate-dropdown">
-			<a title="CAR ELECTRONIC &amp; GPS" href="index.php?page=product-category">CAR ELECTRONIC &#038; GPS</a>
+			<a title="CAR ELECTRONIC &amp; GPS" href="index.php?page=product-category">مکان یاب خودرو</a>
 		</li>
 		<li class="menu-item animate-dropdown">
-			<a title="ACCESORIES" href="index.php?page=product-category">ACCESORIES</a>
+			<a title="ACCESORIES" href="index.php?page=product-category">اکسسوری</a>
 		</li>
 		
 		<li class="techmarket-flex-more-menu-item dropdown">
