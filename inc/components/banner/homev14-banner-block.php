0dee2c24472c425c1b44f4a21198415712006fe8
mahdihasannejad <mahdi2061378@gmail.com>
2018-10-25 16:58:30 +0330

    folder banner

--- inc/components/banner/homev14-banner-block.php
+++ inc/components/banner/homev14-banner-block.php
@@ -4,9 +4,9 @@
 			<div style="background-size: cover; background-position: center center; background-image: url( assets/images/garden/slide-banner1.jpg ); height: 204px;" class="banner-bg">
 				<div class="caption">
 					<div class="banner-info">
-						<h3 class="title">New Arrivals <br> in <strong>Accessories</strong> <br> at Best Prices</h3>
+						<h3 class="title">برترین رسیده ها<br> در <strong>اکسسوری</strong> <br> با بهترین قیمت</h3>
 					</div><!-- .banner-info -->
-					<span class="price"><span class="start_price">from</span>$13.79</span>
+					<span class="price"><span class="start_price">قیمت</span>$13.79</span>
 				</div><!-- .caption -->
 			</div><!-- .banner-bg -->
 		</a>
@@ -17,8 +17,8 @@
 			<div style="background-size: cover; background-position: center center; background-image: url( assets/images/garden/slide-banner2.jpg ); height: 204px;" class="banner-bg">
 				<div class="caption">
 					<div class="banner-info">
-						<h4 class="pretitle">Feat Category</h4>
-						<h3 class="title">catch Best <br><strong>Deals</strong>on the <br> Curved TVs<br>Entertainment</h3>
+						<h4 class="pretitle">دسته نرم افزاری</h4>
+						<h3 class="title">بهترین <br><strong>تخفیف ها</strong>در<br>تلویزیون ها منحنی<br></h3>
 					</div><!-- .banner-info -->
 				</div><!-- .caption -->
 			</div><!-- .banner-bg -->
@@ -30,7 +30,7 @@
 			<div style="background-size: cover; background-position: center center; background-image: url( assets/images/garden/slide-banner3.jpg ); height: 204px;" class="banner-bg">
 				<div class="caption">
 					<div class="banner-info">
-						<h3 class="title"><strong>20% Off Tech</strong> <br> at Ultrabooks, <br> Laptops, Tablets<br>Notebooks &amp;<br>More</h3>
+						<h3 class="title"><strong>20% تخفیف</strong> <br> الترابوک<br> لپتاپ, تبلت<br>نت بوک &amp;<br>بیشتر</h3>
 					</div><!-- .banner-info -->
 				</div><!-- .caption -->
 			</div><!-- .banner-bg -->
@@ -42,8 +42,8 @@
 			<div style="background-size: cover; background-position: center center; background-image: url( assets/images/garden/slide-banner4.jpg ); height: 204px;" class="banner-bg">
 				<div class="caption">
 					<div class="banner-info">
-						<h4 class="pretitle">Best Gift Idea</h4>
-						<h3 class="title">Mini Two Wheel <br><strong>Self Balancing</strong> <br> Scooter</h3>
+						<h4 class="pretitle">بهترین هدیه </h4>
+						<h3 class="title">دوچرخه<br><strong>تعادلی</strong> <br> اسکوتر</h3>
 					</div><!-- .banner-info -->
 					<span class="price">
 						<ins><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">£</span>339.99</span></ins> 
