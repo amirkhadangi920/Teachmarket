f0dfbe9682a1b96bb6815373467f5ae8e184d05a
mohammadreza peykhosh <mohammadreza.peikhosh@gmail.com>
2018-10-25 10:43:09 -0700

    translate feature-list.php

--- inc/blocks/homepage/features-list.php
+++ inc/blocks/homepage/features-list.php
@@ -3,35 +3,35 @@
 		<div class="feature">
 			<div class="media">
 				<i class="feature-icon d-flex mr-3 tm tm-free-delivery"></i>
-				<div class="media-body feature-text"><h5 class="mt-0">Free Delivery</h5><span>from $50</span></div>
+				<div class="media-body feature-text"><h5 class="mt-0">تحویل رایگان</h5><span>از 50 هزار توامن</span></div>
 			</div>
 		</div><!-- .feature -->
 
 		<div class="feature">
 			<div class="media">
 				<i class="feature-icon d-flex mr-3 tm tm-feedback"></i>
-				<div class="media-body feature-text"><h5 class="mt-0">99% Customer</h5><span>Feedbacks</span></div>
+				<div class="media-body feature-text"><h5 class="mt-0">99٪ مشتری</h5><span>باز خورد</span></div>
 			</div><!-- .media -->
 		</div><!-- .feature -->
 
 		<div class="feature">
 			<div class="media">
 				<i class="feature-icon d-flex mr-3 tm tm-free-return"></i>
-				<div class="media-body feature-text"><h5 class="mt-0">365 Days</h5><span>for free return</span></div>
+				<div class="media-body feature-text"><h5 class="mt-0">365 روز</h5><span>برای برگشت رایگان</span></div>
 			</div><!-- .media -->
 		</div><!-- .feature -->
 
 		<div class="feature">
 			<div class="media">
 				<i class="feature-icon d-flex mr-3 tm tm-safe-payments"></i>
-				<div class="media-body feature-text"><h5 class="mt-0">Payment</h5><span>Secure System</span></div>
+				<div class="media-body feature-text"><h5 class="mt-0">پرداخت</h5><span>سیستم ایمنی</span></div>
 			</div><!-- .media -->
 		</div><!-- .feature -->
 
 		<div class="feature">
 			<div class="media">
 				<i class="feature-icon d-flex mr-3 tm tm-best-brands"></i>
-				<div class="media-body feature-text"><h5 class="mt-0">Only Best</h5><span>Brands</span></div>
+				<div class="media-body feature-text"><h5 class="mt-0">فقط بهترین</h5><span>برند ها</span></div>
 			</div><!-- .media -->
 		</div><!-- .feature -->
 	</div><!-- .features -->
