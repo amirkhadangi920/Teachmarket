ec5bdb857fcdcfeda45efd69914d5f41ec44e0e9
mohammadreza peykhosh <mohammadreza.peikhosh@gmail.com>
2018-10-25 16:26:18 -0700

    translate homev4-slider.php

--- inc/blocks/homepage/slider/homev4-slider.php
+++ inc/blocks/homepage/slider/homev4-slider.php
@@ -1,10 +1,10 @@
 <div class="home-v4-slider home-slider" >
 	<div class="slider-1" style="background-image: url(assets/images/slider/home-v4-background.jpg);">
 		<div class="caption">
-			<div class="title">The gift they are <br>wishing for is right here </div>
-			<div class="sub-title">The curved display has a curvature level equivalent to that of a circle, tracks the rounded shape of the eyes better </div>
-			<div class="button">Get Yours now <i class="tm tm-long-arrow-right"></i></div>
-			<div class="bottom-caption">Free shipping on US Terority</div>
+			<div class="title">هدیه ای است که آنها هستند<br>امیدوارم که اینجا هست </div>
+			<div class="sub-title">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </div>
+			<div class="button">یکی خورا بگیرید <i class="tm tm-long-arrow-right"></i></div>
+			<div class="bottom-caption">حمل و نقل رایگان در قلمرو ایالات متحده</div>
 		</div>
 	</div>
 </div>
