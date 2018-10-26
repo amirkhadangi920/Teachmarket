0d56d2d5bd7474543b50333ab6e85dce8ca8fc4a
reza khorrami <rezakhorrami729@gmail.com>
2018-10-26 11:10:09 +0330

    translate page homev11-slider.php

--- inc/blocks/homepage/slider/homev11-slider.php
+++ inc/blocks/homepage/slider/homev11-slider.php
@@ -1,9 +1,9 @@
 <div class="home-v11-slider home-slider" >
 	<div class="slider-1" style="background-image: url(assets/images/slider/home-v11-background.jpg);">
 		<div class="caption">
-			<div class="title">    For a weekend <br>outside the city</div>
-			<div class="sub-title">Comfortable set <br> for him</div>
-			<div class="button">Get Yours now <i class="tm tm-long-arrow-right"></i></div>
+			<div class="title">    برای آخر هفته <br>بیرون شهر</div>
+			<div class="sub-title">مجموعه راحت <br> برای او</div>
+			<div class="button">اکنون شما را دریافت کنید <i class="tm tm-long-arrow-right"></i></div>
 		</div>
 	</div>
 </div>
