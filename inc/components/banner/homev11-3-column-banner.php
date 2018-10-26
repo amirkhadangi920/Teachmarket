af6f94faa94be52f30b17043cf87e578f29ba6a0
mahdihasannejad <mahdi2061378@gmail.com>
2018-10-26 11:44:28 +0330

    debug banner

--- inc/components/banner/homev11-3-column-banner.php
+++ inc/components/banner/homev11-3-column-banner.php
@@ -5,8 +5,8 @@
 				<div style="background-size: cover; background-position: center center; background-image: url( assets/images/banner/11-1.jpg ); height: 174px;" class="banner-bg">
 					<div class="caption">
 						<div class="banner-info">
-							<h4 class="pretitle">Only Branded Sportwear</h4>
-							<h3 class="title">up to 30% off</h3> 
+							<h4 class="pretitle">برند ورزشی</h4>
+							<h3 class="title">تا 30٪ تخفیف</h3> 
 						</div><!-- .banner-info -->
 					</div><!-- .caption -->
 				</div><!-- .banner-bg -->
@@ -18,10 +18,10 @@
 				<div style="background-size: cover; background-position: center center; background-image: url( assets/images/banner/11-2.jpg); height: 174px;" class="banner-bg">
 					<div class="caption">
 						<div class="banner-info">
-							<h4 class="pretitle">The Worlds Best </h4>
-							<h3 class="title">Sport Bras</h3> 
+							<h4 class="pretitle">برترین های چهان</h4>
+							<h3 class="title">ورزشی</h3> 
 						</div><!-- .banner-info -->
-						<span class="banner-action button">Shop now<i class="feature-icon d-flex ml-4 tm tm-long-arrow-right"></i></span>
+						<span class="banner-action button">فروشگاه<i class="feature-icon d-flex ml-4 tm tm-long-arrow-right"></i></span>
 					</div><!-- .caption -->
 				</div><!-- .banner-bg -->
 			</a>
@@ -32,8 +32,8 @@
 				<div style="background-size: cover; background-position: center center; background-image: url( assets/images/banner/11-3.jpg ); height: 174px;" class="banner-bg">
 					<div class="caption">
 						<div class="banner-info">
-							<h4 class="pretitle">Elevate your&nbsp;apparel game</h4>
-							<h3 class="title">to new heights</h3> 
+							<h4 class="pretitle">بالا بردن - بازی لباس خود را</h4>
+							<h3 class="title">ارتفاعات جدید</h3> 
 						</div><!-- .banner-info -->
 					</div><!-- .caption -->
 				</div><!-- .banner-bg -->
