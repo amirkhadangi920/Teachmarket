d01b93992bfa266ea45e739bad586d6d3b0f8271
reza khorrami <rezakhorrami729@gmail.com>
2018-10-26 14:32:42 +0330

    translate page order-received-table.php

--- inc/components/order-received-table.php
+++ inc/components/order-received-table.php
@@ -2,8 +2,8 @@
 
 	<thead>
 		<tr>
-			<th class="woocommerce-table__product-name product-name">Product</th>
-			<th class="woocommerce-table__product-table product-total">Total</th>
+			<th class="woocommerce-table__product-name product-name">محصولات</th>
+			<th class="woocommerce-table__product-table product-total">جمع</th>
 		</tr>
 	</thead>
 
@@ -11,12 +11,12 @@
 		<tr class="woocommerce-table__line-item order_item">
 
 			<td class="woocommerce-table__product-name product-name">
-				<a href="index.php?page=single-product-fullwidth">Snap White Instant Digital Camera in White</a> 
+				<a href="index.php?page=single-product-fullwidth">ضربه محکم و ناگهانی دوربین دیجیتال فوری سفید در سفید</a> 
 				<strong class="product-quantity">× 1</strong>
 			</td>
 
 			<td class="woocommerce-table__product-total product-total">
-				<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>179.99</span>	
+				<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">تومان</span>179.99</span>	
 			</td>
 
 		</tr>
@@ -29,7 +29,7 @@
 			</td>
 
 			<td class="woocommerce-table__product-total product-total">
-				<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>1,197.00</span>
+				<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">تومان</span>1,197.00</span>
 			</td>
 
 		</tr>
@@ -38,20 +38,20 @@
 
 	<tfoot>
 		<tr>
-			<th scope="row">Subtotal:</th>
-			<td><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>1,376.99</span></td>
+			<th scope="row">کل حجم:</th>
+			<td><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">تومان</span>1,376.99</span></td>
 		</tr>
 		<tr>
-			<th scope="row">Shipping:</th>
-			<td><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>100.00</span>&nbsp;<small class="shipped_via">via Normal Delivery</small></td>
+			<th scope="row">حمل دریایی:</th>
+			<td><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">تومان</span>100.00</span>&nbsp;<small class="shipped_via">از طریق تحویل طبیعی</small></td>
 		</tr>
 		<tr>
-			<th scope="row">Payment method:</th>
-			<td>Direct bank transfer</td>
+			<th scope="row">روش پرداخت:</th>
+			<td>انتقال مستقیم بانک</td>
 		</tr>
 		<tr>
-			<th scope="row">Total:</th>
-			<td><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>1,476.99</span></td>
+			<th scope="row">جمع:</th>
+			<td><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">تومان</span>1,476.99</span></td>
 		</tr>
 	</tfoot>
 </table><!-- .woocommerce-table -->
