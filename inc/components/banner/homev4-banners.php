433a100d962cfd949b790e9146eef7cf58f819cd
mohammadreza peykhosh <mohammadreza.peikhosh@gmail.com>
2018-10-26 08:31:22 -0700

    fixe homer-v3-v4 banner

--- inc/components/banner/homev4-banners.php
+++ inc/components/banner/homev4-banners.php
@@ -5,15 +5,9 @@
 				<div style="background-size: cover; background-position: center center; background-image: url( assets/images/banner/3-1.jpg ); height: 259px;" class="banner-bg">
 					<div class="caption">
 						<div class="banner-info">
-<<<<<<< HEAD
 							<h3 class="title">تازه رسیده ها<br> در  <strong>اکسسوری</strong> <br> با بهترین قیمت ها</h3>				
 						</div><!-- .banner-info -->
 						<span class="banner-action button">دیدن همه</span>
-=======
-							<h3 class="title">برترین تازه ها<br> در <strong>اکسسوری</strong> <br> با بهترین قیمت</h3>				
-						</div><!-- .banner-info -->
-						<span class="banner-action button">نمایش همه</span>
->>>>>>> e241bd1c92060a30c86a0bdd38a8d6b3090c2ef0
 					</div><!-- .caption -->
 				</div><!-- .banner-bg -->
 			</a>
@@ -24,15 +18,9 @@
 				<div style="background-size: cover; background-position: center center; background-image: url( assets/images/banner/3-4.jpg ); height: 259px;" class="banner-bg">
 					<div class="caption">
 						<div class="banner-info">
-<<<<<<< HEAD
 							<h3 class="title">گرفتن داغترین<br> <strong>معاملات</strong> در <br>Soundbars منحنی</h3>				
 						</div><!-- .banner-info -->
 						<span class="banner-action button">مرور کردن</span>
-=======
-							<h3 class="title">داغترین<br> <strong>تخفیف ها</strong> در <br> Curved Soundbars.</h3>				
-						</div><!-- .banner-info -->
-						<span class="banner-action button">ادامه</span>
->>>>>>> e241bd1c92060a30c86a0bdd38a8d6b3090c2ef0
 					</div><!-- .caption -->
 				</div><!-- .banner-bg -->
 			</a>
@@ -43,17 +31,10 @@
 				<div style="background-size: cover; background-position: center center; background-image: url( assets/images/banner/3-3.jpg ); height: 259px;" class="banner-bg">
 					<div class="caption">
 						<div class="banner-info">
-<<<<<<< HEAD
 							<h3 class="title"><strong>1000 mAh</strong> <br> پاور بانک </h3>				
 						</div><!-- .banner-info -->
 						<span class="price">96 هزار تومان</span>
 						<span class="banner-action button">الان بخرید</span>
-=======
-							<h3 class="title"><strong>1000 mAh</strong> <br> پاور بانک پرو</h3>				
-						</div><!-- .banner-info -->
-						<span class="price">$34.99</span>
-						<span class="banner-action button">فروشگاهی</span>
->>>>>>> e241bd1c92060a30c86a0bdd38a8d6b3090c2ef0
 					</div><!-- .caption -->
 				</div><!-- .banner-bg -->
 			</a>
